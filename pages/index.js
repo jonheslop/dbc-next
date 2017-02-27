33ee7428c60dc289e1e462a00ef3d76de53e64d5
Jonathan Heslop <jonathan.heslop@digitaslbi.com>
2017-02-27 17:55:03 +0000

    Just working it out

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header role="banner">
+                <h1>Hello World</h1>
+                <p><i>Somehow</i> we’re gonna sell books here</p>
+            </header>
+        )
+    }
+}
+
+export default Header
